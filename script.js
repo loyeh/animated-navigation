b51dac23abf01cd59cdf475eb8c3cb6e0971d86d
Ahmad Asadi Looyeh <asadi.loyeh@gmail.com>
2024-05-07 18:19:19 +0330

    problems fixed and job has been finished

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const btn = document.getElementById("btn");
 let links = document.querySelectorAll(".links");
 const container = document.querySelector(".container");
+const line1 = document.querySelector(".line1");
+const line2 = document.querySelector(".line2");
 links = Array.from(links);
 
 console.log(links);
@@ -11,4 +13,6 @@ btn.addEventListener("click", () => {
   });
   container.classList.toggle("active");
   btn.classList.toggle("active");
+  line1.classList.toggle("active");
+  line2.classList.toggle("active");
 });
